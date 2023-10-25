b705d73f1c1361987f177428b8a9f947714b4d07
AdrianMontes <adrian.montes01@estudiant.upf.edu>
2023-10-25 13:06:50 +0200

    Al clicar sobre un Pokémon, aparecerá su información

--- output/main.js
+++ output/main.js
@@ -49,7 +49,7 @@ function cargarDatos() {
             if (j < lista_actual.length) {
                 marcos[i].style.display = "block";
                 nombres[i].textContent = lista_actual[j].name;
-                imagenes[i].src = lista_actual[j].sprites.front_default;
+                imagenes[i].src = lista_actual[j].sprites.other["official-artwork"].front_default;
                 numeros[i].textContent = "#" + lista_actual[j].id;
             }
             else {
@@ -71,11 +71,12 @@ function pasarLista(valor) {
 }
 function filtrarPokemon() {
     return __awaiter(this, void 0, void 0, function* () {
+        id_inicial = 1;
         let lista_actual = yield Promise.all(lista_pokemon);
         lista_filtros = [];
         if (tipos_filtro.length == 0) {
             for (let i = 0; i < lista_actual.length; i++) {
-                if (lista_actual[i].name.startsWith(buscar.value)) {
+                if (lista_actual[i].name.toLowerCase().startsWith(buscar.value.toLowerCase())) {
                     lista_filtros.push(lista_actual[i]);
                 }
             }
@@ -84,7 +85,7 @@ function filtrarPokemon() {
             for (let i = 0; i < lista_pokemon.length; i++) {
                 for (let j = 0; j < tipos_filtro.length; j++) {
                     if (lista_actual[i].types[0].type.name == tipos_filtro[j] || (lista_actual[i].types[1] && lista_actual[i].types[1].type.name == tipos_filtro[j])) {
-                        if (buscar.value == "" || lista_actual[i].name.startsWith(buscar.value)) {
+                        if (buscar.value == "" || lista_actual[i].name.toLowerCase().startsWith(buscar.value.toLowerCase())) {
                             lista_filtros.push(lista_actual[i]);
                         }
                     }
@@ -92,6 +93,40 @@ function filtrarPokemon() {
             }
         }
         cargarDatos();
+    });
+}
+function cargarPokemon(pokemon) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let pokemon_actual = yield devolverPokemon(pokemon);
+        pokemon_nombre.textContent = pokemon_actual.name;
+        pokemon_imagen.src = pokemon_actual.sprites.other["official-artwork"].front_default;
+        pokemon_numero.textContent = "#" + pokemon_actual.id;
+        pokemon_experiencia.textContent = pokemon_actual.base_experience;
+        pokemon_altura.textContent = pokemon_actual.height;
+        pokemon_peso.textContent = pokemon_actual.weight;
+        pokemon_HP.textContent = pokemon_actual.stats[0].base_stat;
+        pokemon_ataque.textContent = pokemon_actual.stats[1].base_stat;
+        pokemon_defensa.textContent = pokemon_actual.stats[2].base_stat;
+        pokemon_ataque_especial.textContent = pokemon_actual.stats[3].base_stat;
+        pokemon_defensa_especial.textContent = pokemon_actual.stats[4].base_stat;
+        pokemon_velocidad.textContent = pokemon_actual.stats[5].base_stat;
+        pokemon_tipo1.textContent = pokemon_actual.types[0].type.name;
+        if (pokemon_actual.types[1]) {
+            pokemon_tipo2.textContent = pokemon_actual.types[1].type.name;
+        }
+        else {
+            pokemon_tipo2.textContent = "none";
+        }
+    });
+}
+function devolverPokemon(pokemon) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let lista_actual = yield Promise.all(lista_pokemon);
+        for (let i = 0; i < lista_actual.length; i++) {
+            if (lista_actual[i].name == pokemon) {
+                return lista_actual[i];
+            }
+        }
     });
 }
 //Variables
@@ -110,6 +145,22 @@ var imagenes = document.querySelectorAll(".imagen");
 var numeros = document.querySelectorAll(".numero");
 var anterior = document.getElementById("anterior");
 var siguiente = document.getElementById("siguiente");
+var pokemon_cargado;
+var pokemon_nombre = document.getElementById("detalle_nombre");
+var pokemon_imagen = document.getElementById("detalle_imagen");
+var pokemon_numero = document.getElementById("detalle_numero");
+var shiny = document.getElementById("shiny_boton");
+var pokemon_experiencia = document.getElementById("info_base_experience");
+var pokemon_altura = document.getElementById("info_height");
+var pokemon_peso = document.getElementById("info_weight");
+var pokemon_HP = document.getElementById("info_HP");
+var pokemon_ataque = document.getElementById("info_attack");
+var pokemon_defensa = document.getElementById("info_defense");
+var pokemon_ataque_especial = document.getElementById("info_spattack");
+var pokemon_defensa_especial = document.getElementById("info_spdefense");
+var pokemon_velocidad = document.getElementById("info_speed");
+var pokemon_tipo1 = document.getElementById("info_type1");
+var pokemon_tipo2 = document.getElementById("info_type2");
 cargarDatos();
 //Eventos
 buscar.addEventListener("input", function () {
@@ -127,6 +178,12 @@ tipos.forEach((boton) => {
             tipos_filtro.push(boton.textContent);
         }
         filtrarPokemon();
+    });
+});
+marcos.forEach(function (marco) {
+    marco.addEventListener("click", function () {
+        pokemon_cargado = marco.querySelector(".nombre").textContent;
+        localStorage.setItem("pokemon_cargado", pokemon_cargado);
     });
 });
 anterior.addEventListener("click", function () {
--- typescript/main.ts
+++ typescript/main.ts
@@ -35,7 +35,7 @@ async function cargarDatos() {
         if (j < lista_actual.length) {
             marcos[i].style.display = "block";
             nombres[i].textContent = lista_actual[j].name;
-            imagenes[i].src = lista_actual[j].sprites.front_default;
+            imagenes[i].src = lista_actual[j].sprites.other["official-artwork"].front_default;
             numeros[i].textContent = "#" + lista_actual[j].id; 
         }
         else {
@@ -57,12 +57,13 @@ function pasarLista(valor: number) {
 }
 
 async function filtrarPokemon() {
+    id_inicial = 1;
     let lista_actual = await Promise.all(lista_pokemon);
     lista_filtros = [];
 
     if (tipos_filtro.length == 0) {
         for (let i: number = 0; i < lista_actual.length; i++) {
-            if (lista_actual[i].name.startsWith(buscar.value)) {
+            if (lista_actual[i].name.toLowerCase().startsWith(buscar.value.toLowerCase())) {
                 lista_filtros.push(lista_actual[i]);
             }
         }
@@ -70,7 +71,7 @@ async function filtrarPokemon() {
         for (let i: number = 0; i < lista_pokemon.length; i++) {
             for (let j: number = 0; j < tipos_filtro.length; j++) {
                 if (lista_actual[i].types[0].type.name == tipos_filtro[j] || (lista_actual[i].types[1] && lista_actual[i].types[1].type.name == tipos_filtro[j])) {
-                    if (buscar.value == "" || lista_actual[i].name.startsWith(buscar.value)) {
+                    if (buscar.value == "" || lista_actual[i].name.toLowerCase().startsWith(buscar.value.toLowerCase())) {
                         lista_filtros.push(lista_actual[i]);
                     }
                 }
@@ -79,6 +80,40 @@ async function filtrarPokemon() {
     }
     
     cargarDatos();
+}
+
+async function cargarPokemon(pokemon: string) {
+    let pokemon_actual: any = await devolverPokemon(pokemon);
+
+    pokemon_nombre.textContent = pokemon_actual.name;
+    pokemon_imagen.src = pokemon_actual.sprites.other["official-artwork"].front_default;
+    pokemon_numero.textContent = "#" + pokemon_actual.id;
+    pokemon_experiencia.textContent = pokemon_actual.base_experience;
+    pokemon_altura.textContent = pokemon_actual.height;
+    pokemon_peso.textContent = pokemon_actual.weight;
+    pokemon_HP.textContent = pokemon_actual.stats[0].base_stat;
+    pokemon_ataque.textContent = pokemon_actual.stats[1].base_stat;
+    pokemon_defensa.textContent = pokemon_actual.stats[2].base_stat;
+    pokemon_ataque_especial.textContent = pokemon_actual.stats[3].base_stat;
+    pokemon_defensa_especial.textContent = pokemon_actual.stats[4].base_stat;
+    pokemon_velocidad.textContent = pokemon_actual.stats[5].base_stat;
+    pokemon_tipo1.textContent = pokemon_actual.types[0].type.name;
+    if (pokemon_actual.types[1]) {
+        pokemon_tipo2.textContent = pokemon_actual.types[1].type.name;
+    } else {
+        pokemon_tipo2.textContent = "none";
+    }
+    
+}
+
+async function devolverPokemon(pokemon: string): Promise<any> {
+    let lista_actual = await Promise.all(lista_pokemon);
+
+    for (let i: number = 0; i < lista_actual.length; i++) {
+        if (lista_actual[i].name == pokemon) {
+            return lista_actual[i];
+        }
+    }
 }
 
 //Variables
@@ -100,6 +135,23 @@ var numeros = document.querySelectorAll(".numero");
 var anterior: any = document.getElementById("anterior");
 var siguiente: any = document.getElementById("siguiente");
 
+var pokemon_cargado: string;
+var pokemon_nombre: any = document.getElementById("detalle_nombre");
+var pokemon_imagen: any = document.getElementById("detalle_imagen");
+var pokemon_numero: any = document.getElementById("detalle_numero");
+var shiny: any = document.getElementById("shiny_boton");
+var pokemon_experiencia: any = document.getElementById("info_base_experience");
+var pokemon_altura: any = document.getElementById("info_height");
+var pokemon_peso: any = document.getElementById("info_weight");
+var pokemon_HP: any = document.getElementById("info_HP");
+var pokemon_ataque: any = document.getElementById("info_attack");
+var pokemon_defensa: any = document.getElementById("info_defense");
+var pokemon_ataque_especial: any = document.getElementById("info_spattack");
+var pokemon_defensa_especial: any = document.getElementById("info_spdefense");
+var pokemon_velocidad: any = document.getElementById("info_speed");
+var pokemon_tipo1: any = document.getElementById("info_type1");
+var pokemon_tipo2: any = document.getElementById("info_type2");
+
 cargarDatos();
 
 //Eventos
@@ -118,6 +170,13 @@ tipos.forEach((boton: any) => {
             tipos_filtro.push(boton.textContent);
         }
         filtrarPokemon();
+    });
+});
+
+marcos.forEach(function (marco: any) {
+    marco.addEventListener("click", function () {
+        pokemon_cargado = marco.querySelector(".nombre").textContent;
+        localStorage.setItem("pokemon_cargado", pokemon_cargado);
     });
 });
 
